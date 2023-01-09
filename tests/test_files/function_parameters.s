c28e64fa4decdf067d1013b73a63b70c47eb2740
Zaph-x <35431127+Zaph-x@users.noreply.github.com>
2023-01-09 19:46:11 +0100

    added test

---
+++ tests/test_files/function_parameters.s
@@ -0,0 +1,68 @@
+	.arch armv8-a
+	.file	"function_parameters.c"
+	.text
+	.align	2
+	.global	int_function
+	.type	int_function, %function
+int_function:
+.LFB0:
+	.cfi_startproc
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	w0, [sp, 12]
+	mov	w0, 1
+	str	w0, [sp, 12]
+	nop
+	add	sp, sp, 16
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	int_function, .-int_function
+	.align	2
+	.global	other_int_function
+	.type	other_int_function, %function
+other_int_function:
+.LFB1:
+	.cfi_startproc
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	str	x0, [sp, 8]
+	ldr	x0, [sp, 8]
+	mov	w1, 1
+	str	w1, [x0]
+	nop
+	add	sp, sp, 16
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	other_int_function, .-other_int_function
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB2:
+	.cfi_startproc
+	stp	x29, x30, [sp, -32]!
+	.cfi_def_cfa_offset 32
+	.cfi_offset 29, -32
+	.cfi_offset 30, -24
+	mov	x29, sp
+	str	wzr, [sp, 28]
+	str	wzr, [sp, 24]
+	ldr	w0, [sp, 28]
+	bl	int_function
+	add	x0, sp, 24
+	bl	other_int_function
+	mov	w0, 0
+	ldp	x29, x30, [sp], 32
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE2:
+	.size	main, .-main
+	.ident	"GCC: (Debian 8.3.0-6) 8.3.0"
+	.section	.note.GNU-stack,"",@progbits
